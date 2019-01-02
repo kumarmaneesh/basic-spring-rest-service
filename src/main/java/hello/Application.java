700d7271a2997bce903cc168054f48416992af48
Maneesh <maneesh.kumar1@sc.com>
2019-01-02 17:51:41 +0800

    Initial Commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 1556780 on 1/2/2019.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
